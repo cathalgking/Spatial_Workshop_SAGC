54673c2c5c3221b2f27e010d54777604382e80f1
Cathal King <32261323+cathalgking@users.noreply.github.com>
2024-02-20 23:23:22 +1030

    Update conf.py
    ck

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
-    'style_nav_header_background': #ffff99
+    'style_nav_header_background': '#808000'
 }
 
 # -- Options for EPUB output
